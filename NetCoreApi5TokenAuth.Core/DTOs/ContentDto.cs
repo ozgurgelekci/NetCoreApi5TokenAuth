bbbb9e0254460ba5dc3f7d2b9bc0c30103f54787
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 23:33:28 +0300

    ContentDto, DtoMapper, ObjectMapper

---
+++ NetCoreApi5TokenAuth.Core/DTOs/ContentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCoreApi5TokenAuth.Core.DTOs
+{
+    public class ContentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ NetCoreApi5TokenAuth.Service/Mappings/DtoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using NetCoreApi5TokenAuth.Core.DTOs;
+using NetCoreApi5TokenAuth.Core.Entities.Concrete;
+
+namespace NetCoreApi5TokenAuth.Service.Mappings
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ContentDto, Content>().ReverseMap();
+
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
