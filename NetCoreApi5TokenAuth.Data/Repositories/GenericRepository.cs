39375076d40f482ccd0588d76cf5d39cb0127a95
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 21:31:03 +0300

    GenericRepository

---
+++ NetCoreApi5TokenAuth.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NetCoreApi5TokenAuth.Core.Entities.Abstract;
+using NetCoreApi5TokenAuth.Core.Repositories.Abstract;
+using NetCoreApi5TokenAuth.Data.DbContexts;
+
+namespace NetCoreApi5TokenAuth.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        private readonly DbContext _dbContext;
+        private DbSet<TEntity> _dbSet;
+
+        public GenericRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbContext.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+    }
+}
