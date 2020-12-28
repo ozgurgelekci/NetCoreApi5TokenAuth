0d3e855909e9c7a3af4d8705ee0d3c2c8d092847
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 05:21:04 +0300

    IUnitOfWork

---
+++ NetCoreApi5TokenAuth.Core/UnitOfWork/Abstract/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace NetCoreApi5TokenAuth.Core.UnitOfWork.Abstract
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
