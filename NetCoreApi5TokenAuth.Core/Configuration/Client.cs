47c3dd14e31a5f7d1b9f2ad635a1f7c46ef28919
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 04:58:07 +0300

    ITokenService and Client

---
+++ NetCoreApi5TokenAuth.Core/Configuration/Client.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace NetCoreApi5TokenAuth.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        public List<string> Audiences { get; set; }
+    }
+}
---
+++ NetCoreApi5TokenAuth.Core/Services/Abstract/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using NetCoreApi5TokenAuth.Core.Configuration;
+using NetCoreApi5TokenAuth.Core.DTOs;
+using NetCoreApi5TokenAuth.Core.Entities.Concrete;
+
+namespace NetCoreApi5TokenAuth.Core.Services.Abstract
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        ClientTokenDto CreateClientToken(Client client);
+    }
+}
