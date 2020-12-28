0aa705f6e2c60b590f3b6d3b1e0bf1058351b713
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 05:05:54 +0300

    IAuthenticationService, LoginDto and ClientLoginDto

---
+++ NetCoreApi5TokenAuth.Core/DTOs/ClientLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCoreApi5TokenAuth.Core.DTOs
+{
+    public class ClientLoginDto
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ NetCoreApi5TokenAuth.Core/Services/Abstract/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NetCoreApi5TokenAuth.Core.DTOs;
+using SharedLibrary.DTOs;
+
+namespace NetCoreApi5TokenAuth.Core.Services.Abstract
+{
+    public interface IAuthenticationService
+    {
+        Task<ResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<ResponseDto<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<ResponseDto<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<ResponseDto<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
