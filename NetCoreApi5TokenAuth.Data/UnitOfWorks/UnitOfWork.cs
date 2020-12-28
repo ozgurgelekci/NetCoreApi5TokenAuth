fbb5835a195224fae8b3646f13c40fa29c437304
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 21:33:38 +0300

    UnitOfWork

---
+++ NetCoreApi5TokenAuth.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreApi5TokenAuth.Core.UnitOfWork.Abstract;
+using NetCoreApi5TokenAuth.Data.DbContexts;
+using System.Threading.Tasks;
+
+namespace NetCoreApi5TokenAuth.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
