a9e3e0c62395ebe6eee94278f178c92595aee2c6
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 04:36:25 +0300

    Create ResponseDto

---
+++ SharedLibrary/DTOs/ResponseDto.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.DTOs
+{
+    public class ResponseDto<T> where T : class
+    {
+        public T Data { get; set; }
+        public int StatusCode { get; private set; }
+        public ErrorDto Error { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Error = errorDto,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        public static ResponseDto<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+
+            return new ResponseDto<T> { Error = errorDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+    }
+}
