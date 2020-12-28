1da6827935143c1c77a6f48f044e9e068cc328ff
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-28 05:14:39 +0300

    CreateUserDto, UserAppDto and IUserService

---
+++ NetCoreApi5TokenAuth.Core/Services/Abstract/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using NetCoreApi5TokenAuth.Core.DTOs;
+using SharedLibrary.DTOs;
+
+namespace NetCoreApi5TokenAuth.Core.Services.Abstract
+{
+    public interface IUserService
+    {
+        Task<ResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto userAppDto);
+
+        Task<ResponseDto<UserAppDto>> GetByUserNameAsync(string userName);
+    }
+}
