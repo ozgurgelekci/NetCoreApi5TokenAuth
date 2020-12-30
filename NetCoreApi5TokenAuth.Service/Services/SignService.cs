1aba4334a0bad8e1742a91e1c37cdbe975991fb9
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-31 01:08:49 +0300

    SingService and CustomTokenOption

---
+++ NetCoreApi5TokenAuth.Service/Services/SignService.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace NetCoreApi5TokenAuth.Service.Services
+{
+    public static class SignService
+    {
+        public static SecurityKey GetSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
